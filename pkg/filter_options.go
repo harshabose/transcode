3bc4d2ebc65ef1251f967bed84136c0774f3d03f
harshabose <harshavardhanbose@gmail.com>
2025-03-12 06:06:30 +0530

    general commit

--- pkg/filter_options.go
+++ pkg/filter_options.go
@@ -56,6 +56,9 @@ func withVideoSetFilterContextParameters(decoder *Decoder) func(*Filter) error {
 		filter.srcContextParams.SetTimeBase(decoder.decoderContext.TimeBase())
 		filter.srcContextParams.SetWidth(decoder.decoderContext.Width())
 
+		filter.srcContextParams.SetColorSpace(decoder.decoderContext.ColorSpace())
+		filter.srcContextParams.SetColorRange(decoder.decoderContext.ColorRange())
+
 		return nil
 	}
 }
